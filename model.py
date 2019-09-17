ee2b7ca465b63d07b9fd508300fc3dba7b4772f5
ChunYen Chang <fikrusnk@gmail.com>
2019-09-17 17:47:25 +0800

    FIRST COMMIT

---
+++ model.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy.types import *
+
+
+BaseModel = declarative_base()
+
+
+class RestaurantInf(BaseModel):
+    __tablename__ = 'RestaurantInf'
+    restaurant_id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(512))
+    address = Column(String(512))
+    photo = Column(String(512))
+    
+    
+engine = create_engine("mysql+mysqlconnector://id:pw@ec2-54-68-247-255.us-west-2.compute.amazonaws.com:3306/restaurant")
+
+BaseModel.metadata.create_all(engine)
+    
