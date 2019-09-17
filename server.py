690e9ec4463afffe4c70c1847a7103c7ecd67a34
ChunYen Chang <fikrusnk@gmail.com>
2019-09-18 02:13:24 +0800

    FEAT: Add HTTP authentication

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify, request
-from model import BaseModel, RestaurantInf
+from flask import Flask, jsonify, request, g
+from model import BaseModel, RestaurantInf, User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from find_restaurant import findlocation, retrive_restaurant_inf
+from passlib.apps import custom_app_context as pwd_context
+from flask_httpauth import HTTPBasicAuth
 
 
 google_key = 'your google API key'
@@ -15,10 +17,45 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 
 
+@auth.verify_password
+def verify_password(username, password):
+	user = session.query(User).filter_by(account = username).first()
+	if not user:
+		return False
+
+	account = user.account
+	hash_passowrd = user.password
+	
+	if not pwd_context.verify(password, hash_passowrd):
+		return False
+	g.user = user
+	return True
+
+
+@app.route('/user', methods = ['POST'])
+def user_register():
+	user_name = request.form['username']
+	user_password= request.form['userpassword']
+	user_password_hash = pwd_context.encrypt(user_password)
+	
+	user = session.query(User).filter_by(account = user_name).first()
+	if user:
+		return 'The user exists'
+
+	new_user = User(account=user_name,
+			password=user_password_hash)
+	session.add(new_user)
+	session.commit()
+
+	return 'Already registered a new user'
+
+
 @app.route('/AllRestaurantInf', methods = ['GET', 'POST'])
+@auth.login_required
 def get_all_restaurants_inf():
 	if request.method == 'GET':
 		all_restaurant = session.query(RestaurantInf).all()
@@ -42,6 +79,7 @@ def get_all_restaurants_inf():
 
 
 @app.route('/RestaurantInf/<int:onerestaurantid>', methods = ['GET','PUT', 'DELETE'])
+@auth.login_required
 def get_update_delete_restaurant(onerestaurantid):
 	if request.method == 'GET':
 		restaurant = session.query(RestaurantInf).filter_by(restaurant_id = onerestaurantid).one()
