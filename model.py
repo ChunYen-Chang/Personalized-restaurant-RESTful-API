0a148a0f6dbe770905e4dcec9d48ac218e4efb5b
ChunYen Chang <fikrusnk@gmail.com>
2019-09-19 01:00:29 +0800

    DOCS: Put documentation and comment

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# import packages
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
@@ -5,9 +6,11 @@ from sqlalchemy import Column
 from sqlalchemy.types import *
 
 
+# declare the basemodel
 BaseModel = declarative_base()
 
 
+# define the User and RestaurantInf class
 class User(BaseModel):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -21,9 +24,9 @@ class RestaurantInf(BaseModel):
     name = Column(String(512))
     address = Column(String(512))
     photo = Column(String(512))
-    
-    
+ 
+ 
+# create user and Restaurant table in Mysql database    
 engine = create_engine("mysql+mysqlconnector://id:pw@ec2-54-68-247-255.us-west-2.compute.amazonaws.com:3306/restaurant")
-
 BaseModel.metadata.create_all(engine)
     
